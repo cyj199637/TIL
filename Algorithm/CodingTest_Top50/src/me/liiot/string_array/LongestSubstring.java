e2b7e6cb94cbb5a6d3bc21d95fa60d0e0f3fb34e
cyj199637 <cyj199637@gmail.com>
2021-01-10 00:09:26 +0900

    210109 CodingTest_Top50 | 10 ~ 16

---
+++ Algorithm/CodingTest_Top50/src/me/liiot/string_array/LongestSubstring.java
@@ -0,0 +1,49 @@
+package me.liiot.string_array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+12) Longest Substring With At Most Two Distinct
+String s가 주어졌을 때, s에서 최대 2개의 문자만 포함하면서 가장 긴 연속된 문자열 t의 길이를 찾으세요.
+ */
+public class LongestSubstring {
+
+    public static void main(String[] args) {
+
+        String s1 = "eceba";
+        String s2 = "ccaabbb";
+        String s3 = "bdkekefiee";
+
+        System.out.println(solve(s1));
+        System.out.println(solve(s2));
+        System.out.println(solve(s3));
+    }
+
+    private static int solve(String s) {
+
+        // counter: 문자열에 포함된 문자 개수
+        int start = 0, end = 0, length = 0, counter = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        while (end<s.length()) {
+            char endChar = s.charAt(end);
+            map.put(endChar, (map.getOrDefault(endChar, 0) + 1));
+            if (map.get(endChar) == 1) counter++;   // 새로운 문자가 추가되었다는 의미이므로 counter 증가
+            end++;
+
+            while (counter > 2) {
+                char startChar = s.charAt(start);
+                map.put(startChar, (map.get(startChar) - 1));   // 앞에서 세었던 문자를 제거
+                if (map.get(startChar) == 0) counter--;     // 앞에서 센 문자를 다 제거하면 문자 개수 감소
+                start++;
+            }
+
+            // 이전에 세었던 문자가 map에 없어도 나중에 저장된 문자가 포함된 문자열보다 길었다면 이 값은 갱신되지 않음
+            // 문자열 자체를 리턴하는게 아니라 가장 긴 문자열의 길이만 리턴하면 되기 때문에 가능
+            length = Math.max(length, end-start);
+        }
+
+        return length;
+    }
+}
---
+++ Algorithm/CodingTest_Top50/src/me/liiot/string_array/MaximumSubarray.java
@@ -0,0 +1,34 @@
+package me.liiot.string_array;
+
+/*
+13) Maximum Subarray
+정수 배열 nums가 주어졌을 때, nums에서 합계가 가장 큰 연속된 부분 배열 (최소 하나의 숫자 포함)을 찾아 그 합계를 반환합니다.
+
+- 원래 이 문제에서 어떤 값을 더해야 큰 지 알아볼 때에는 세 가지 경우가 있다.
+  1. 현재 원소 뺴고 기존 원소(or 합)만 남기기 / 2. 기존 원소(or 합) + 현재 원소 / 3. 기존 원소(or 합) 빼고 현재 원소만 남기기
+ 그러나 1번의 경우, 그 값이 세 가지 경우에서 가장 크더라도 그 다음 원소와 비교할 때에는 연속적인 부분 집합이
+ 되지 못하기 때문에 문제 조건을 만족할 수 없다. 따라서, 여기서는 2, 3번 경우만 고려해야 한다.
+ 그리고 어차피 1번 경우가 가장 큰 값이었다면 max에 남아있기 때문에 현재 원소와 비교할 필요가 없음
+ */
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        System.out.println(solve(nums));
+    }
+
+    private static int solve(int[] nums) {
+
+        int max = nums[0];
+        int sum = nums[0];
+
+        for (int i=1; i<nums.length; i++) {
+            sum = Math.max(sum+nums[i], nums[i]);   // 기존 원소(or 합) + 현재 원소 vs 현재 원소
+            max = Math.max(max, sum);   // 기존 원소(or 합)가 가장 큰 값이었다면 max값이 바뀌지 않을 것임
+        }
+
+        return max;
+    }
+}
